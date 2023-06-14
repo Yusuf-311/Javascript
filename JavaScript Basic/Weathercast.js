057373371369d3b3755ad66e45e70c724c67da0e
Moh Yusuf <93629262+Yusuf-311@users.noreply.github.com>
2023-06-14 16:47:24 +0700

    Javascript

---
+++ JavaScript Basic/Weathercast.js
@@ -0,0 +1,9 @@
+let kelvin = 0; // Kelvin value is 45
+
+let celcius = kelvin - 273.15; // Celsius value is Kelvin - 273.15
+
+let fahrenheit = (celcius * (9/5)) + 32; // Fahrenheit value
+
+fahrenheit = Math.floor(fahrenheit); // Rounding down Fahrenheit to the nearest whole number
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`); // Output: The temperature is <fahrenheit> degrees Fahrenheit
